d44b2779afcf2f426a0b8730a04ab9960989528c
Joseph Rodrigo <113199438+itsjorigo@users.noreply.github.com>
2023-06-06 16:26:06 +0530

    1st commit - push, pop functions used to create my_stack list

--- Stacks_and_Queues.py
+++ Stacks_and_Queues.py
@@ -1,1 +1,12 @@
-print("This is about Stacks and Queues")
+def push(stack, value):
+    stack.append(value)
+
+def pop(stack):
+    return stack.pop()
+
+my_stack = []
+push(my_stack, 'a')
+push(my_stack, 'b')
+push(my_stack, 'c')
+
+print(my_stack)
