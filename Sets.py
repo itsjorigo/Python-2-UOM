fb63b7dac8dd93cf831bc838a10555567f7d5d8f
Joseph Rodrigo <113199438+itsjorigo@users.noreply.github.com>
2023-06-12 23:44:24 -0700

    2nd commit - pop, remove, discard, add, update attributes.

--- Sets.py
+++ Sets.py
@@ -1,7 +1,32 @@
 fruits = {'apple', 'orange', 'banana'}
-print(fruits)
+# print(fruits)
 
 fruits_1 = set(['apple', 'orange', 'banana'])
 
-print(len(fruits))
+# print(len(fruits))
+# print(fruits)
+
+# for item in fruits:
+#     print(item)
+
+fruits.add('mango')
+# print(fruits)
+
+fruits.update(['mango', 'pineapple'])
+# print(fruits)
+
+fruits.remove('apple')
 print(fruits)
+
+fruits.remove('mango')
+print(fruits)
+
+fruits.add('mango')
+print(fruits)
+
+fruits.discard('mango')
+print(fruits)
+
+fruits.pop()
+print(fruits)
+
